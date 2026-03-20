8342b5c3e9eef6f6e8b657d04d753a374b7fe211
Ubuntu <ubuntu@ip-172-31-43-179.ap-south-1.compute.internal>
2026-03-20 07:42:26 +0000

    add

--- config/db.js
+++ config/db.js
@@ -1,21 +1,23 @@
 import mongoose from "mongoose";
 
 export async function connectDb() {
-
-    try {
-        await mongoose.connect(process.env.MONGODB_URI)
-        console.log("Db connected")
-    } catch (err) {
-        console.log(err.message)
-        process.exit(1)
-    }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("DB connected");
+  } catch (err) {
+    console.log(err.message);
+    process.exit(1);
+  }
 }
 
-
+// Graceful shutdown
 process.on("SIGINT", async () => {
-    await client.close()
-    console.log("client Disconnected")
-    process.exit(0)
-})
-
-
+  try {
+    await mongoose.connection.close();
+    console.log("DB disconnected");
+    process.exit(0);
+  } catch (err) {
+    console.log("Error closing DB:", err.message);
+    process.exit(1);
+  }
+});
